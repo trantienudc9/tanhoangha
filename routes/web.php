08fd59ae38abb53642245a47cc541f990b5d8725
trantienudc9 <tienductvd.com>
2024-07-28 22:16:16 +0700

    bsj

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,35 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuppliesController;
 use App\Http\Controllers\ImageProductController;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', [SuppliesController::class, 'index'])->name('product.index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('trang-chu', [SuppliesController::class, 'index'])->name('product.index');
 Route::get('them-san-pham/{id?}', [SuppliesController::class, 'create_product'])->name('product.create');
 Route::post('store-product', [SuppliesController::class, 'store_product'])->name('product.store');
 Route::put('update-product', [SuppliesController::class, 'update_product'])->name('product.update');
@@ -17,8 +39,6 @@ Route::get('san-pham/{kind_product_type}/{product_type}', [SuppliesController::c
 Route::get('gioi-thieu', [SuppliesController::class, 'introduce_products'])->name('product.introduce');
 Route::get('tuyen-dung', [SuppliesController::class, 'recruitment_products'])->name('product.recruitment');
 Route::get('lien-he', [SuppliesController::class, 'contact_products'])->name('product.contact');
-
-// infor_produc
 Route::get('thong-so-san-pham/{id}', [SuppliesController::class, 'parameters'])->name('product.parameters');
 Route::put('update_parameters', [SuppliesController::class, 'update_parameters'])->name('product.update_parameters');
 
@@ -29,3 +49,4 @@ Route::post('store-background', [ImageProductController::class, 'store_backgroun
 Route::put('update-background', [ImageProductController::class, 'update_background'])->name('image.update_background');
 Route::delete('delete-background', [ImageProductController::class, 'delete_background'])->name('image.delete_background');
 
+require __DIR__.'/auth.php';
