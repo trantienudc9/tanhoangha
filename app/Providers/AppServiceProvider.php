5a564a9789eac832c0c6b3aef5e27f49ad8bfc3a
trantienudc9 <tienductvd@gmail.com>
2024-08-07 11:40:22 +0700

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\ProductType;
 use App\Models\KindProductType;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,7 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->share('productTypesAll', ProductType::with(['kinds.supplies','supplies'])->get());
-        view()->share('kindProducts', KindProductType::get());
+        try {
+            view()->share('productTypesAll', ProductType::with(['kinds.supplies', 'supplies'])->get());
+            view()->share('kindProducts', KindProductType::get());
+        } catch (\Exception $e) {
+            // Xử lý lỗi, ví dụ: log lỗi hoặc hiển thị thông báo
+            \Log::error($e->getMessage());
+        }
     }
 }
